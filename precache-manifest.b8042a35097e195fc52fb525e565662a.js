8d249e4d5a2a40d724c5c4451640d181ca750f83
manourez <37008006+manourez@users.noreply.github.com>
2019-05-06 22:18:43 +0800

    Add files via upload

---
+++ precache-manifest.b8042a35097e195fc52fb525e565662a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "673e78c36c64b723bb88ff97f69f9e4f",
+    "url": "/static/media/qrcode.673e78c3.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a9fc33d41fcabb8ff55c",
+    "url": "/static/js/main.f6a816be.chunk.js"
+  },
+  {
+    "revision": "927bea3139740bbd23c8",
+    "url": "/static/js/2.aef0b9f2.chunk.js"
+  },
+  {
+    "revision": "a9fc33d41fcabb8ff55c",
+    "url": "/static/css/main.f0361f04.chunk.css"
+  },
+  {
+    "revision": "927bea3139740bbd23c8",
+    "url": "/static/css/2.f809678e.chunk.css"
+  },
+  {
+    "revision": "19923454d314fe8de1579df85e59ab56",
+    "url": "/index.html"
+  }
+];
